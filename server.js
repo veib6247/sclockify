7666d6de288d399cdca70311c9511d6364c78e7e
Bryan Olandres <ozoneblacklight@outlook.com>
2023-06-13 18:31:04 +0800

    changed route method to post, added railway port support

--- server.js
+++ server.js
@@ -11,14 +11,25 @@ const fastify = Fastify({
 })
 
 // Declare a route
-fastify.get('/', async (request, reply) => {
-  return { areYou: process.env.READ_THIS }
+fastify.post('/', async (request, reply) => {
+  console.log(request.body)
 })
 
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 })
+    // run on local mode
+    if (process.env.SERVER_MODE === 'TEST') {
+      console.log('Server running in DEV mode')
+      await fastify.listen({ port: 3000 })
+    } else {
+      console.log('Server running in PROD mode')
+      // use port provided by railway when hosted
+      fastify.listen({
+        host: '0.0.0.0',
+        port: process.env.PORT,
+      })
+    }
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
